dabc6ac3a438bcadb134e298951faac90184c906
trweiss17 <142759304+trweiss17@users.noreply.github.com>
2024-04-23 21:12:55 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,32 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
-} 
+ private int leastSteps;
+ private int allSteps;
+ private int allDays;
+ private int actDays;
+
+ public StepTracker(int goal){
+   leastSteps= goal;
+  allSteps= 0;
+  allDays= 0;
+  actDays=0;
+ }
+ public void addDailySteps(steps){
+   allSteps= allSteps+ steps;
+  allSteps++;
+  if(steps>= leastSteps){
+   actDays++;
+  }
+ }
+ public int activeDays(actDays){
+return actDays;
+ }
+ public double averageSteps(){
+  if(allDays==0){
+   return 0.0;
+  }
+  return (double) allSteps/allDays;
+ }
+}
+                           
